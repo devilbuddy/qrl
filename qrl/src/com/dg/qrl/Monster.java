c578d44d13b2ffdc7557eec8f331c857f90b02a8
magnus.ornebring <magnus.ornebring@king.com>
2014-07-02 22:08:50 +0200

    Added some monsters

---
+++ qrl/src/com/dg/qrl/Monster.java
@@ -0,0 +1,20 @@
+package com.dg.qrl;
+
+import com.dg.qrl.World.Actor;
+
+public class Monster extends Entity implements Actor {
+
+	private final World world;
+	
+	public Monster(World world) {
+		super();
+		this.world = world;
+	}
+	
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
