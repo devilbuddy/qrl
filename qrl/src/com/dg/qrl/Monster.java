f33886669c0264fbd164d76d20fc98fe61c9c4e2
magnus.ornebring <magnus.ornebring@king.com>
2014-07-03 15:16:34 +0200

    some type of state AI

--- qrl/src/com/dg/qrl/Monster.java
+++ qrl/src/com/dg/qrl/Monster.java
@@ -6,15 +6,21 @@ import com.dg.qrl.World.Actor;
 
 public class Monster extends Entity implements Actor {
 
-	private final World world;
 	
-	private final class Brain {
-		private Random random = new Random();
+	
+	private interface State {
+		void act(Monster monster, World world);
+	}
+			
+	private static final Random RANDOM = new Random();
+
+	private static final State moveRandomlyState  = new State() {
+		@Override
 		public void act(Monster monster, World world) {
-			if(random.nextBoolean()) {
+			if(RANDOM.nextBoolean()) {
 				Point position = new Point();
 				position.set(monster.getPosition());
-				switch(random.nextInt(5)) {
+				switch(RANDOM.nextInt(5)) {
 				case 0:
 					position.setX(position.getX() + 1);
 					break;
@@ -28,24 +34,54 @@ public class Monster extends Entity implements Actor {
 					position.setY(position.getY() - 1);
 					break;
 				}
-				
+
 				if(world.isPassable(position.getX(), position.getY())) {
 					world.moveEntity(monster, position);
 				}
 			}
 		}
+	};
+	
+	private static final State idleState  = new State() {
+		@Override
+		public void act(Monster monster, World world) {
+			
+		}
+	};
+	
+	private enum StateKey {
+		IDLE(idleState),
+		MOVE_RANDOMLY(moveRandomlyState);
+		
+		public final State state;
+		private StateKey(State state) {
+			this.state = state;
+		}
 	}
 	
-	private Brain brain = new Brain();
+
+	
+	private final World world;
+	private boolean canAct = false;
+	private State state;
 	
 	public Monster(World world) {
 		super();
 		this.world = world;
+		setState(StateKey.IDLE);
 	}
 	
 	@Override
 	public void act() {
-		brain.act(this, world);
+		canAct = true;
+		state.act(this, world);
+		canAct = false;
 	}
-
+	
+	void setState(StateKey stateKey) {
+		this.state = stateKey.state;
+		if(canAct) {
+			state.act(this, world);
+		}
+	}
 }
