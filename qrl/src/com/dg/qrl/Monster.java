d381e297438eb545f3bec94a9bd8ffd865cba015
magnus.ornebring <magnus.ornebring@king.com>
2014-07-03 13:18:57 +0200

    moving monsters x/y

--- qrl/src/com/dg/qrl/Monster.java
+++ qrl/src/com/dg/qrl/Monster.java
@@ -14,12 +14,21 @@ public class Monster extends Entity implements Actor {
 			if(random.nextBoolean()) {
 				Point position = new Point();
 				position.set(monster.getPosition());
-				
-				if(random.nextBoolean()) {
-					position.setX(position.getX() + 1);	
-				} else {
+				switch(random.nextInt(5)) {
+				case 0:
+					position.setX(position.getX() + 1);
+					break;
+				case 1:
 					position.setX(position.getX() - 1);
+					break;
+				case 2:
+					position.setY(position.getY() + 1);
+					break;
+				case 3:
+					position.setY(position.getY() - 1);
+					break;
 				}
+				
 				if(world.isPassable(position.getX(), position.getY())) {
 					world.moveEntity(monster, position);
 				}
